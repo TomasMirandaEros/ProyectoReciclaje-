3395c664ea879e8d3297ae9b74be8867d90cb462
TomasMirandaEros <135926219+TomasMirandaEros@users.noreply.github.com>
2023-06-19 23:27:24 -0300

    movimiento

--- scrips/movimiento.cs
+++ scrips/movimiento.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class movimiento : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private new Rigidbody rigidbody;
+    public float velocidad;
+    // para modificar el valor de la sensebilidad 
+    public Vector2 sensebilidadDelMouse;
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    //void del movimiento 
+    void UpdateMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = (transform.forward * vertical + transform.right * horizontal).normalized;
+        Vector3 velocity = direction * velocidad;
+        velocity.y = rigidbody.velocity.y;
+        rigidbody.velocity = velocity;
     }
 
-    // Update is called once per frame
+    void UpdateMouseLook()
+    {
+        float horizontal = Input.GetAxis("Mouse X");
+        float vertical = Input.GetAxis("Mouse Y");
+
+        if (horizontal != 0)
+        {
+            transform.Rotate(0, horizontal, 0);
+        }
+
+    }
+
+
     void Update()
     {
-        
+        UpdateMovement();
+        UpdateMouseLook();
     }
 }
